2871f8f6984283b9a78bd01e84eae884183c4dca
Brandon Huffman <bt_huffman@msn.com>
2019-10-24 11:26:07 -0600

    Was able to get all the pictures to drag and drop and rotate using proper inline CSS in the divs

--- reactdragndrop/src/components/Board.js
+++ reactdragndrop/src/components/Board.js
@@ -6,66 +6,74 @@ function Board() {
     return (
         <main className="board">
             <BoardTile id="boardTile-1" col="1" row="1" className="boardTile">
-                <Card id="card-1"  draggable="true" 
-                    // topColor={2}
-                    //     rightColor={4}
-                    //     bottomColor={5}
-                    //     leftColor={3}
-                        >
+                <Card
+                    id="card-1" draggable="true"
+                    topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}
+                >
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-2" col="2" row="1" className="boardTile">
-                <Card id="card-2"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card 
+                id="card-2" 
+                draggable="true"
+                 topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-3" col="3" row="1" className="boardTile">
-                {/* <Card id="card-3"  draggable="true">
+                <Card
+                 id="card-3"  draggable="true"                  topColor={2}
+                 rightColor={4}
+                 bottomColor={5}
+                 leftColor={3}>>
             <p>Card Three</p>
-          </Card> */}
+          </Card>
             </BoardTile>
             <BoardTile id="boardTile-4" col="1" row="2" className="boardTile">
-                <Card id="card-4"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-4" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-5" col="2" row="2" className="boardTile">
-                <Card id="card-5"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-5" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-6" col="3" row="2" className="boardTile">
-                <Card id="card-6"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-6" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-7" col="1" row="3" className="boardTile">
-                <Card id="card-7"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-7" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-8" col="2" row="3" className="boardTile">
-                <Card id="card-8"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-8" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
             <BoardTile id="boardTile-9" col="3" row="3" className="boardTile">
-                <Card id="card-9"  draggable="true" topColor={2}
-                        rightColor={4}
-                        bottomColor={5}
-                        leftColor={3}>
+                <Card id="card-9" draggable="true" topColor={2}
+                    rightColor={4}
+                    bottomColor={5}
+                    leftColor={3}>
                 </Card>
             </BoardTile>
         </main>
--- reactdragndrop/src/components/Card.js
+++ reactdragndrop/src/components/Card.js
@@ -7,112 +7,93 @@ import orangeTile from "./orangeTile.jpg"
 import purpleTile from "./purpleTile.jpg"
 import redTile from "./redTile.jpg"
 import whiteTile from "./whiteTile.jpg"
-
 class Card extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            rotation: 45,
-            colors: [props.topColor, props.rightColor, props.bottomColor, props.leftColor]
-        }
-
-        this.rotate = this.rotate.bind(this)
-    }
-
-    rotate() {
-        let temp = this.state.colors
-        let tempColor = temp.pop()
-        temp.unshift(tempColor);
-
-        let newColors = temp
-        this.setState({
-            colors: newColors
-        })
-    }
-
-    getLanternImage(num) {
-        let image = null;
-        switch (num) {
-            case 1:
-                image = blackTile
-                break
-            case 2:
-                image = blueTile
-                break
-            case 3:
-                image = greenTile
-                break
-            case 4:
-                image = orangeTile
-                break
-            case 5:
-                image = purpleTile
-                break
-            case 6:
-                image = redTile
-                break
-            case 7:
-                image = whiteTile
-                break;
-        }
-
-        return image
-    }
-
-    dragStart = e => {
-        const target = e.target;
-
-        e.dataTransfer.setData('card_id', target.id);
-
-        // Allows it to be visible when we drag it.
-        setTimeout(() => {
-            target.style.display = "none";
-        }, 0);
-    }
-
-    dragOver = e => {
-        e.stopPropagation();
-    }
-    render() {
-        const top = this.getLanternImage(this.state.colors[0])
-        const right = this.getLanternImage(this.state.colors[1])
-        const bottom = this.getLanternImage(this.state.colors[2])
-        const left = this.getLanternImage(this.state.colors[3])
-
-        return (
-            <div
-            // id={this.props.id}
-            // className={this.props.className}
-
-            > {this.props.children}
-                <div
-                    id={this.props.id}
-                    className="parentContainer"
-                    draggable={this.props.draggable}
-                    onDragStart={this.dragStart}
-                    onDragOver={this.dragOver}
-                >
-                    <div className="parent"
-                        onClick={this.rotate}>
-                       
-
-                    <div className="topTri" style={{ backgroundImage: top}}>
-                            {/* <img src={top} alt="top lanterns" /> */}
-                        </div>
-                        <div className="rightTri">
-                            <img src={right} alt="right lanterns" />
-                        </div>
-                        <div className="leftTri">
-                            <img src={left} alt="left lanterns" />
-                        </div>
-                        <div className="bottomTri">
-                            <img src={bottom} alt="bottom lanterns" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+   constructor(props) {
+       super(props)
+       this.state = {
+           rotation: 45,
+           colors: [props.topColor, props.rightColor, props.bottomColor, props.leftColor]
+       }
+       this.rotate = this.rotate.bind(this)
+   }
+   rotate() {
+       let temp = this.state.colors
+       let tempColor = temp.pop()
+       temp.unshift(tempColor);
+       let newColors = temp
+       this.setState({
+           colors: newColors
+       })
+   }
+   getLanternImage(num) {
+       let image = null;
+       switch (num) {
+           case 1:
+               image = blackTile
+               break
+           case 2:
+               image = blueTile
+               break
+           case 3:
+               image = greenTile
+               break
+           case 4:
+               image = orangeTile
+               break
+           case 5:
+               image = purpleTile
+               break
+           case 6:
+               image = redTile
+               break
+           case 7:
+               image = whiteTile
+               break;
+       }
+       return image
+   }
+   dragStart = e => {
+       const target = e.target;
+       e.dataTransfer.setData('card_id', target.id);
+       // Allows it to be visible when we drag it.
+       setTimeout(() => {
+           target.style.display = "none";
+       }, 0);
+   }
+   dragOver = e => {
+       e.stopPropagation();
+   }
+   render() {
+       const top = this.getLanternImage(this.state.colors[0])
+       const right = this.getLanternImage(this.state.colors[1])
+       const bottom = this.getLanternImage(this.state.colors[2])
+       const left = this.getLanternImage(this.state.colors[3])
+       return (
+           <div
+           // id={this.props.id}
+           // className={this.props.className}
+           > {this.props.children}
+               <div
+                   id={this.props.id}
+                   className="parentContainer"
+                   draggable={this.props.draggable}
+                   onDragStart={this.dragStart}
+                   onDragOver={this.dragOver}
+               >
+                   <div className="parent"
+                       onClick={this.rotate}>
+                   <div className="topTri" style={{ backgroundImage: "url("+ top + ")"}}>
+                       </div>
+                       <div className="rightTri" style={{ backgroundImage: "url("+ right + ")"}}>
+                       </div>
+                       <div className="leftTri" style={{ backgroundImage: "url("+ left + ")"}}>
+                       </div>
+                       <div className="bottomTri" style={{ backgroundImage: "url("+ bottom + ")"}}>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       )
+   }
 }
-
 export default Card
